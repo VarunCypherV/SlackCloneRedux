9914f81328059ab11a2c5f41e0a30d1367f4f353
VarunCypherV <varunvk2004@gmail.com>
2023-08-18 15:37:45 +0530

    all done V1

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -20,7 +20,9 @@ const Login = () => {
     return (
       <LoginContainer>
         <LoginInnerContainer>
-          {/* Your other components */}
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Slack_icon_2019.svg/2048px-Slack_icon_2019.svg.png"></img>
+          <br/>
+          <h1>SLACK </h1>
           <Button onClick={signIn}>Sign In With Google</Button>
         </LoginInnerContainer>
       </LoginContainer>
@@ -45,10 +47,11 @@ box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
     >img {
         object-fit: contain; //maintainaspect ratio
         height : 100px;
-        margin-bottom : 40px;
+        margin-bottom : 30px;
+
     }
     >button {
-        margin-top : 50px;
+        margin-top : 60px;
         text-transform : inherit !important;
         background-color: #0a8d48 !important;
         color : white;
